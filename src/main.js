ef20961871221397a88d0727dd921cf0e86bdb3c
kishan-incquet <kishan.r@incquet.com>
2025-03-21 17:01:54 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+import vuetify from './plugins/vuetify'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  pinia,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
